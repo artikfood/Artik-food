5cbd8a8642aae4b0bd61145eeb2041e7e190e23b
artikfood <edulik28@yandex.ru>
2026-01-21 04:53:50 +0400

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,31 @@
 const CACHE_NAME = "artik-food-v1";
-const urlsToCache = [
+const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "/style.css",
-  "/script.js",
   "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
+  "/logo.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
